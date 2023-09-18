a1d78ef08d100bd65a1d069d5a6eded385a41b9a
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-18 15:25:13 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/miniprn/MainActivity.java
@@ -0,0 +1,18 @@
+package com.daghlas.miniprn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red));
+
+    }
+}
