9844ab9e7e070842790874bbf9f17b1dbd48f948
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-20 09:43:31 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/miniprn/PRNumber.java
@@ -0,0 +1,41 @@
+package com.daghlas.miniprn;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class PRNumber extends AppCompatActivity {
+
+    ImageView back;
+    Button proceed;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prnumber);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red));
+
+        back = findViewById(R.id.backButton);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        proceed = findViewById(R.id.proceed);
+        proceed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PRNumber.this, Receipt.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
