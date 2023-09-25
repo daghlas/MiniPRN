e93e072556a2e0870362f11cb36d1d52a471835d
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-25 11:12:48 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/QRCode.java
+++ app/src/main/java/com/daghlas/miniprn/QRCode.java
@@ -1,14 +1,22 @@
 package com.daghlas.miniprn;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class QRCode extends AppCompatActivity {
 
     ImageView back;
+    Button scanQR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +30,36 @@ public class QRCode extends AppCompatActivity {
                 onBackPressed();
             }
         });
+
+        scanQR = findViewById(R.id.scanQR);
+        scanQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                scanQRCode();
+            }
+        });
     }
+
+    private void scanQRCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Press volume up button to on flash light");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLaunch.launch(options);
+    }
+
+    ActivityResultLauncher <ScanOptions> barLaunch = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(QRCode.this);
+            builder.setTitle("Scan result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("PAY", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 }
