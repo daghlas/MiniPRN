caeb658e5e85ea5950f69befe4306d0da3601819
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-26 14:59:44 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/miniprn/QRPay.java
@@ -0,0 +1,14 @@
+package com.daghlas.miniprn;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QRPay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrpay);
+    }
+}
