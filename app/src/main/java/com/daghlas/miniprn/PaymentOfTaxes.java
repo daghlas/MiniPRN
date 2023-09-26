db71a125641610d6b6da525565b138818dc1df7a
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-09-26 09:14:11 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/PaymentOfTaxes.java
+++ app/src/main/java/com/daghlas/miniprn/PaymentOfTaxes.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PaymentOfTaxes extends AppCompatActivity {
 
