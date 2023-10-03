675b0412c218912e381e4de532feaf8619d84ce0
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-03 15:17:05 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/Constants.java
+++ app/src/main/java/com/daghlas/miniprn/Constants.java
@@ -5,9 +5,9 @@ public class Constants {
     public static final int READ_TIMEOUT = 60 * 1000;
     public static final int WRITE_TIMEOUT = 60 * 1000;
     public static final String BASE_URL = "https://sandbox.safaricom.co.ke/";
-    public static final String BUSINESS_SHORT_CODE = "572572"; //KRA BUSINESS PAYBILL NUMBER
+    public static final String BUSINESS_SHORT_CODE = "174379"; //TO BE REPLACED WITH KRA BUSINESS PAYBILL NUMBER
     public static final String PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
     public static final String TRANSACTION_TYPE = "CustomerPayBillOnline";
-    public static final String PARTYB = "572572"; // KRA BUSINESS PAYBILL NUMBER
+    public static final String PARTYB = "174379"; // KRA BUSINESS PAYBILL NUMBER, AGAIN
     public static final String CALLBACKURL = "http://mpesa-requestbin.herokuapp.com/14vfhf01";
 }
