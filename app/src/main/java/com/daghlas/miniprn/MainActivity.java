cda725dd15fcaa5db47ea06fdaf95b35871e9b09
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-05 09:31:43 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/MainActivity.java
+++ app/src/main/java/com/daghlas/miniprn/MainActivity.java
@@ -1,16 +1,20 @@
 package com.daghlas.miniprn;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     CardView paymentOfTaxes;
+    TextView developer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         paymentOfTaxes = findViewById(R.id.paymentOfTaxes);
+        developer = findViewById(R.id.developer);
 
         getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red));
 
@@ -26,6 +31,30 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, PaymentOfTaxes.class);
                 startActivity(intent);
+            }
+        });
+
+        developer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setIcon(R.mipmap.ic_launcher_custom);
+                builder.setTitle(R.string.app_name);
+                builder.setMessage("This application is purely a test version still under development. " +
+                        "This version of the app is for a proposal objective to the KRA regarding " +
+                        "streamlining of tax payment procedures by adding QR Code scanning and prompting of the " +
+                        "Safaricom STK Push Service as a payment procedure.\n\nAny similarities portrayed to the official " +
+                        "KRA MService App are completely unintentional and only strive to accomplish the intended " +
+                        "demonstrations for this app.\nThis app is not for commercial purposes and cannot be sold " +
+                        "or reproduced without the developer's consent.\n\nDevelopers contact: \nPhone: 0723325631" +
+                        "\nEmail: daghlaskaire58@gmail.com");
+                builder.setCancelable(false);
+                builder.setPositiveButton("DONE", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
             }
         });
 
