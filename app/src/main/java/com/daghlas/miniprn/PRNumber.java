03bd18da647b62f3831d654e7464a2ce58a32026
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-06 09:03:09 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/PRNumber.java
+++ app/src/main/java/com/daghlas/miniprn/PRNumber.java
@@ -1,13 +1,16 @@
 package com.daghlas.miniprn;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class PRNumber extends AppCompatActivity {
 
@@ -30,12 +33,23 @@ public class PRNumber extends AppCompatActivity {
         });
 
         proceed = findViewById(R.id.proceed);
-        proceed.setEnabled(false);
+        //proceed.setEnabled(false);
         proceed.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(PRNumber.this, Receipt.class);
-                startActivity(intent);
+                //Intent intent = new Intent(PRNumber.this, Receipt.class);
+                //startActivity(intent);
+                AlertDialog.Builder builder = new AlertDialog.Builder(PRNumber.this);
+                builder.setTitle(R.string.app_name);
+                builder.setIcon(R.mipmap.ic_launcher_custom);
+                builder.setMessage("PRN PAYMENT \nINTEGRATION COMING SOON");
+                builder.setCancelable(false);
+                builder.setPositiveButton("OKAY", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                }).show();
             }
         });
     }
