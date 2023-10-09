717e3f083230debdd0b6a59102c971e403595523
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-09 08:27:41 +0300

    initial commit

---
+++ app/src/main/java/com/daghlas/miniprn/PHModel.java
@@ -0,0 +1,38 @@
+package com.daghlas.miniprn;
+
+public class PHModel {
+
+    String datePaid, amountPaid, referenceNumber, phoneNumber, description;
+
+    public PHModel(String datePaid, String amountPaid, String referenceNumber, String phoneNumber, String description) {
+        this.datePaid = datePaid;
+        this.amountPaid = amountPaid;
+        this.referenceNumber = referenceNumber;
+        this.phoneNumber = phoneNumber;
+        this.description = description;
+    }
+
+    public PHModel(){
+
+    }
+
+    public String getDatePaid() {
+        return datePaid;
+    }
+
+    public String getAmountPaid() {
+        return amountPaid;
+    }
+
+    public String getReferenceNumber() {
+        return referenceNumber;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
