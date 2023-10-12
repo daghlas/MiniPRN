8da8ef0f94cbbe1d8eb313b57f24da3a2db45ed2
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-12 09:23:10 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/Utils.java
+++ app/src/main/java/com/daghlas/miniprn/Utils.java
@@ -15,15 +15,18 @@ public class Utils {
     public static String sanitizePhoneNumber(String phone) {
         if (phone.equals("")) {
             return "";
-        }if (phone.length() < 11 & phone.startsWith("0")) {
+        }
+        if (phone.length() < 11 & phone.startsWith("0")) {
             String p = phone.replaceFirst("^0", "254");
             return p;
-        }if (phone.length() == 13 && phone.startsWith("+")) {
+        }
+        if (phone.length() == 13 && phone.startsWith("+")) {
             String p = phone.replaceFirst("^+", "");
             return p;
         }
         return phone;
     }
+
     public static String getPassword(String businessShortCode, String passkey, String timestamp) {
         String str = businessShortCode + passkey + timestamp;
         //encode the password to Base64
