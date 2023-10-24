d66d69dbea6654f0cbe13d448f7e9af94b85065f
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-24 09:45:28 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/Interceptor/AccessTokenInterceptor.java
+++ app/src/main/java/com/daghlas/miniprn/Interceptor/AccessTokenInterceptor.java
@@ -12,7 +12,7 @@ import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class AccessTokenInterceptor implements Interceptor{
+public class AccessTokenInterceptor implements Interceptor {
 
     public AccessTokenInterceptor() {
 
