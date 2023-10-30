b1f2cba764b7c70f210dea4c4f687a3d04aab35f
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-30 17:00:32 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/Constants.java
+++ app/src/main/java/com/daghlas/miniprn/Constants.java
@@ -8,6 +8,6 @@ public class Constants {
     public static final String BUSINESS_SHORT_CODE = "174379"; //TO BE REPLACED WITH KRA BUSINESS PAYBILL NUMBER
     public static final String PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
     public static final String TRANSACTION_TYPE = "CustomerPayBillOnline";
-    public static final String PARTYB = "174379"; // KRA BUSINESS PAYBILL NUMBER, AGAIN
+    public static final String PARTYB = "174379"; // KRA BUSINESS PAY BILL NUMBER, AGAIN
     public static final String CALLBACKURL = "http://mpesa-requestbin.herokuapp.com/14vfhf01";
 }
