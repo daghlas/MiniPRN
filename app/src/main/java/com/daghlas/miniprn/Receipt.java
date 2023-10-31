8b60d8fc5a174f7833f7480cfb46aad1f67cc8e4
Daghlas Kaire <daghlaskaire58@gmail.com>
2023-10-31 15:36:32 +0300

    initial commit

--- app/src/main/java/com/daghlas/miniprn/Receipt.java
+++ app/src/main/java/com/daghlas/miniprn/Receipt.java
@@ -1,6 +1,7 @@
 package com.daghlas.miniprn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
 
@@ -10,5 +11,7 @@ public class Receipt extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receipt);
+
+        getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.red));
     }
 }
